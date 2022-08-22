99a42307523b5ed3b1a5000498f606904381f234
Andrea Martinez <andrea.maort@gmail.com>
2022-08-22 19:01:06 +0200

    added formSteps html tested

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,19 +4,19 @@ import Form from "react-bootstrap/Form";
 function FormSteps() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="name">
         <Form.Label>Name</Form.Label>
         <Form.Control type="text" placeholder="What's your name?" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="surname">
         <Form.Label>Surname</Form.Label>
         <Form.Control type="text" placeholder="What's your surname?" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="birthdate">
         <Form.Label>Birthdate</Form.Label>
         <Form.Control type="date" placeholder="Enter your birthdate" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
